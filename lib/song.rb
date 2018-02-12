de831a4685485abcf5f7040805428663a5945698
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 16:37:42 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'set'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    self.name = name
+    self.artist = artist
+    self.genre = genre
+    @@count += 1
+
+    @@genres << genre
+  end
+
+  def count
+    @@count
+  end
+
+  def genres
+    Array.new(Set.new(@@genres))
+  end
+
+
+
+
+
+
+  
